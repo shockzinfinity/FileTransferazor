f1a913ed06e0b22e565ebc2ad1c3edda3e1af851
shockz-mbp <shockz@me.com>
2021-09-20 21:54:38 +0900

    modify model, email sender
    but still error in emailsender because of gmail auth process

---
+++ Server/Migrations/20210920123041_originalFilename.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FileTransferazor.Server.Migrations
+{
+    public partial class originalFilename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalFileName",
+                table: "FileStorageDatas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalFileName",
+                table: "FileStorageDatas");
+        }
+    }
+}
