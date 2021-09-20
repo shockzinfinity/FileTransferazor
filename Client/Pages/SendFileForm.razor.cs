84339d0a08b733e51f98d5516ecbae81845ba4fb
shockz-mbp <shockz@me.com>
2021-09-20 12:33:22 +0900

    create basic form

---
+++ Client/Pages/SendFileForm.razor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FileTransferazor.Client.Shared;
+using FileTransferazor.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Logging;
+using MudBlazor;
+
+namespace FileTransferazor.Client.Pages
+{
+    public partial class SendFileForm
+    {
+        private FileSendData _fileSendData = new FileSendData();
+        [Inject] public IDialogService Dialog { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public ISnackbar Snackbar { get; set; }
+        private List<IBrowserFile> loadedFiles = new();
+        private bool _isLoading;
+        [Inject] public ILogger<SendFileForm> Logger { get; set; }
+
+        public async Task HandleValidSubmit()
+        {
+            await ExecuteDialog();
+        }
+
+        public async Task HandleSelected(InputFileChangeEventArgs e)
+        {
+            _isLoading = true;
+            loadedFiles.Clear();
+
+            foreach (var file in e.GetMultipleFiles())
+            {
+                try
+                {
+                    loadedFiles.Add(file);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError($"Files: {file.Name} Error: {ex.Message}");
+                }
+            }
+
+            Snackbar.Add("image selected", Severity.Info);
+            _isLoading = false;
+        }
+
+        private async Task ExecuteDialog()
+        {
+            var parameters = new DialogParameters
+            {
+                { "Content", "You have successfully created a file data." },
+                { "ButtonColor", Color.Primary },
+                { "ButtonText", "Ok" }
+            };
+
+            var dialog = Dialog.Show<DialogNotification>("Success", parameters);
+            var result = await dialog.Result;
+            if (!result.Cancelled)
+            {
+                bool.TryParse(result.Data.ToString(), out bool shouldNavigate);
+                if (shouldNavigate) NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
---
+++ Server/Migrations/20210920021743_FileData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FileTransferazor.Server.Migrations
+{
+    public partial class FileData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileSendDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReceiverEmail = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileSendDatas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FileStorageDatas",
+                columns: table => new
+                {
+                    FileSendDataId = table.Column<int>(type: "int", nullable: false),
+                    FileUri = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileStorageDatas", x => x.FileSendDataId);
+                    table.ForeignKey(
+                        name: "FK_FileStorageDatas_FileSendDatas_FileSendDataId",
+                        column: x => x.FileSendDataId,
+                        principalTable: "FileSendDatas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileStorageDatas");
+
+            migrationBuilder.DropTable(
+                name: "FileSendDatas");
+        }
+    }
+}
